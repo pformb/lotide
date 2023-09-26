db4ab3ff2e11f51a8d288caa371c8a4e738508c8
Paul Formby <paulformby@icloud.com>
2023-09-26 18:02:41 +0000

    countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+// countLetters.js
+
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+
+  const results = {};
+  for (let letter of string) {
+    // console.log(letter)
+    if (letter === " ") {
+      continue;
+    }
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+
+};
+
+
+console.log(countLetters("lighthouse in the house"));

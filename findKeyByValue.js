9168a361ea3255d2f69f40d5839a2c0d5d507538
Paul Formby <paulformby@icloud.com>
2023-09-27 05:33:45 +0000

    findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,48 @@
+// findKeyByValue
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// search for key(genre) by value(tvShow)
+const findKeyByValue = function(object, value) {
+// loop through keys(genres) and assign keys(genres) to variable category
+  for (const category of Object.keys(object)) {
+    // if given value(tvShow) is found in the object(bestTVShowsByGenre) by loop
+    if (value === object[category]) {
+      // return category(genre)
+      return category;
+    }
+  }
+};
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+  
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+

b63f8cf9223fbf35ae96c334b556b62e933cdd6a
Paul Formby <paulformby@icloud.com>
2023-09-28 03:53:44 +0000

    added comments to files

--- head.js
+++ head.js
@@ -1,5 +1,6 @@
+
+// Test Function:
 const assertEqual = function(actual, expected) {
-  
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,10 +8,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Function to return the value at 0 index position in an array 
 const head = function(array) {
+  // Return head of array also known as the value at 0 index position
   return array[0];
 };
 
-
+// Test Code:
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- tail.js
+++ tail.js
@@ -1,5 +1,5 @@
+// Test Function:
 const assertEqual = function(actual, expected) {
-  
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,18 +7,21 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Function to return everything except the 0 index position in an array
 const tail = function(array) {
+  // Declare a variable called newArray as an empty array
   const newArray = [];
+  // Loop through array starting at index position 1 (skipping 0)
   for (let i = 1; i < array.length; i++) {
+    // Push array[i] which is all the other values in array other than 0 to newArray
     newArray.push(array[i]);
   }
-  console.log(newArray);
   return newArray;
-
 };
-// Test Case: Check the original array
+
+// Test Code:
+
 const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
-// tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(words)
 
 

c720fd5627197ee5c7e9f12ef2a4de9d35d97768
Paul Formby <paulformby@icloud.com>
2023-10-11 20:49:22 +0000

    cleaned spaces and added middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,7 +2,6 @@ const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 // Test Code:
-
 assertArraysEqual(middle([1]), []); // => []
 assertArraysEqual(middle([1, 2]), []); // => []
 assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
--- test/tailTest.js
+++ test/tailTest.js
@@ -2,7 +2,6 @@ const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail');
 
 // Test Code:
-
 assertArraysEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), [ 'Lighthouse', 'Labs' ])
 assertArraysEqual(tail([1, 2, 3]), [ 2, 3 ])
 
